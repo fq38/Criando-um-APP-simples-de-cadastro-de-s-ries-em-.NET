80aadf834781bcbc5119d89d25abd816901ba038
felipe quirino <fq38@live.com>
2021-08-28 20:51:17 -0300

    Adicionar arquivos de projeto.

---
+++ Dio.Series/Classes/SerieRepositorio.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using Dio.Series.Interfaces;
+using Dio.Series;
+
+
+namespace DIO.Series
+{
+	public class SerieRepositorio : IRepositorio<Serie>
+	{
+		private List<Serie> listaSerie = new List<Serie>();
+		public void Atualiza(int id, Serie objeto)
+		{
+			listaSerie[id] = objeto;
+		}
+
+		public void Exclui(int id)
+		{
+			listaSerie[id].Excluir();
+		}
+
+		public void Insere(Serie objeto)
+		{
+			listaSerie.Add(objeto);
+		}
+
+		public List<Serie> Lista()
+		{
+			return listaSerie;
+		}
+
+		public int ProximoId()
+		{
+			return listaSerie.Count;
+		}
+
+		public Serie RetornaPorId(int id)
+		{
+			return listaSerie[id];
+		}
+
+        public Serie RetornaPortId(int id)
+        {
+			return listaSerie[id];
+
+		}
+	}
+}
